d92e93c6e191e4ca343c713e59282621bde89a3b
beatrizcristiny <beatrizcrispires@gmail.com>
2026-03-25 08:49:59 -0300

    php

---
+++ Ex1.php
@@ -0,0 +1,13 @@
+<?php
+$primeironome = ["Ana", "Bia", "David", "Gabriela", "Rodrigo"];
+$segundonome = ["Magalhões", "Cristiny", "Peres", "Santana", "Campos"];
+echo "$primeironome[0]  $segundonome[0]<br>" ;
+echo "$primeironome[1]  $segundonome[1]<br>" ;
+echo "$primeironome[2]  $segundonome[2]<br>" ;
+echo "$primeironome[3]  $segundonome[3]<br>" ;
+echo "$primeironome[4]  $segundonome[4]<br>" ;
+
+ 
+
+
+?>
