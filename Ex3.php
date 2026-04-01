45054c094f86340aa386bd3e52404a9d4903607b
beatrizcristiny <beatrizcrispires@gmail.com>
2026-04-01 08:38:50 -0300

    Ex3

---
+++ Ex3.php
@@ -0,0 +1,6 @@
+<?php
+$cliente = [["id" => 1, "nome" => "Bia", "saldo" => 5000,00], ["id" => 4, "nome" => "David", "saldo" => 1500,00], ["id" => 6, "nome" => "Ana", "saldo" => 3000,00]];
+echo $cliente[0]['nome'];
+
+
+?>
