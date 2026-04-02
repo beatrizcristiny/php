91b717d40f922cdaa9d0a89c1e8797ef1b6201c9
beatrizcristiny <beatrizcrispires.com>
2026-04-02 09:33:49 -0300

    ex1 aula 2 e criando subpasta

--- Ex1.php
+++ Ex1.php
@@ -1,13 +1,8 @@
 <?php
-$primeironome = ["Ana", "Bia", "David", "Gabriela", "Rodrigo"];
-$segundonome = ["Magalhões", "Cristiny", "Peres", "Santana", "Campos"];
-echo "$primeironome[0]  $segundonome[0]<br>" ;
-echo "$primeironome[1]  $segundonome[1]<br>" ;
-echo "$primeironome[2]  $segundonome[2]<br>" ;
-echo "$primeironome[3]  $segundonome[3]<br>" ;
-echo "$primeironome[4]  $segundonome[4]<br>" ;
-
- 
+$cidades = ["Marília", "São Paulo", "Bauru", "Fortaleza", "Lins"];
+foreach ($cidades as $cidade){
+    echo $cidade. "<br>";
+}
 
 
 ?>
