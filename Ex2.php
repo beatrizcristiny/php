69d459c6bdcae8c1e73f8fa8daf080fa1a427045
beatrizcristiny <beatrizcrispires.com>
2026-04-02 09:39:02 -0300

    ex2 aula 2

---
+++ Ex2.php
@@ -0,0 +1,8 @@
+<?php
+$escola = ["nome" => "Bia", "idade" => "16", "curso" => "Desenvolvimento de sistemas"];
+foreach ($escola as $aluno => $valor){
+    echo $aluno . ":" .$valor . "<br>";
+}
+
+
+?>
