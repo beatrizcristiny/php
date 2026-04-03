cd86b54b0853452eced5f9bf63e7edaab1e71dbe
beatrizcristiny <beatrizcrispires@gmailcom>
2026-04-03 20:41:45 -0300

    tentando deixar bonitinho

--- Ex3.php
+++ Ex3.php
@@ -2,7 +2,7 @@
 
 $produtos = [["nome" => "Uva", "preco" => 4,00], ["nome" => "Banana", "preco" => 5,00], ["nome" => "Morango", "preco" => 8,50]];
 foreach ($produtos as $todos => $eles){
-     echo $todos . ":" .$eles["nome"] . $eles["preco"]. "<br>";
+     echo $todos . ":" .$eles["nome"].  "R$:" . $eles["preco"]. "<br>";
 } 
 
 
