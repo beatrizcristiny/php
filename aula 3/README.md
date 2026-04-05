d23af4aeaf6132e67ca396ee97c29e6ba4f56b69
beatrizcristiny <beatrizcrispires@gmail.com>
2026-04-05 20:19:01 -0300

    aula 3 e pegando os cods da apostila aula 4

---
+++ aula 3/README.md
@@ -0,0 +1,5 @@
+ex1- Organizar é essencial pra não ficar tudo bagunçado, e facilita procurar arquivos, deletar e corrigi-los.
+
+ex2-criado
+
+ex3- Na pasta data guarda os dados que vai simular um bd, includes guarda os arquivos repetidos ou reutilizaveis e em pages ficam as telas que o cliente ou usuario acessa.
--- aula 3/data/dados.php
+++ aula 3/data/dados.php
@@ -1,4 +1,20 @@
 <?php
-$usuarios = [];
+$usuarios = [
+    [
+        "usuario" => "cliente",
+        "senha" => "123",
+        "perfil" => "cliente"
+    ],
+    [
+        "usuario" => "empresa",
+        "senha" => "123",
+        "perfil" => "empresa"
+    ],
+    [
+        "usuario" => "admin",
+        "senha" => "123",
+        "perfil" => "proprietario"
+    ]
+];
 $contas = [];
 ?>
