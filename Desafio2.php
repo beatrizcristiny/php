4bce96fe8f14b4fce0ae3895aefa9f50f2c8a023
beatrizcristiny <beatrizcrispires@gmail.com>
2026-04-08 08:48:04 -0300

    aula 6 desafio 2

---
+++ Desafio2.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema Financeiro</title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+   <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class ="card text-bg-success">
+        <div class="card-body">
+            <h5 class="card-title">Contas a receber</h5>
+            <p class="card-text">R$ 1.200,00</p>
+        </div>
+        </div>
+        <div class="card text-bg-danger">
+        <div class="card-body"></div>
+        <h5 class="card-title">Contas a pagar</h5>
+        <p class="card-text"> R$ 900.00</p>
+        <p class="card-text"> R$ 900.00</p>
+        
+        </div>
+        <div class="card text-bg-secondary">
+        <div class= "card-body"></div>
+        <h5 class = "card-title">Saldo total</h5>
+        <p class= "card-text">R$ 300.00</p>
+
+
+        </div>
+ 
+
+        
+
+</body>
+</html>
+
+
